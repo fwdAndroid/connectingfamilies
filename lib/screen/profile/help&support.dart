7000d7e18faa7f72b66a65ba9054cd4577cbee89
fwdAndroid <fwdkaleem@gmail.com>
2024-10-23 22:55:04 +0500

    Report and Help

--- lib/screen/profile/help&support.dart
+++ lib/screen/profile/help&support.dart
@@ -1,8 +1,16 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectingfamilies/provider/language_provider.dart';
+import 'package:connectingfamilies/service/storage.dart';
 import 'package:connectingfamilies/uitls/colors.dart';
+import 'package:connectingfamilies/uitls/image_picker.dart';
 import 'package:connectingfamilies/widget/save_button.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 class HelpSupport extends StatefulWidget {
   const HelpSupport({super.key});
@@ -12,10 +20,14 @@ class HelpSupport extends StatefulWidget {
 }
 
 class _HelpSupportState extends State<HelpSupport> {
+  TextEditingController _messageController = TextEditingController();
+  Uint8List? image;
+  bool isloading = false;
   @override
   Widget build(BuildContext context) {
     final languageProvider = Provider.of<LanguageProvider>(context); // Access
     final size = MediaQuery.of(context).size;
+    var uuid = Uuid().v4();
 
     return Scaffold(
       appBar: AppBar(
@@ -47,6 +59,7 @@ class _HelpSupportState extends State<HelpSupport> {
             SizedBox(height: 16),
             // TextField for message input
             TextField(
+              controller: _messageController,
               maxLines: 6,
               decoration: InputDecoration(
                 hintText: 'Type your message',
@@ -57,33 +70,62 @@ class _HelpSupportState extends State<HelpSupport> {
             ),
             SizedBox(height: 16),
             // Button for adding a screenshot
-            ElevatedButton.icon(
-              onPressed: () {
-                // Handle adding screenshot
-              },
-              icon: Icon(Icons.add_photo_alternate_outlined),
-              label: Text(languageProvider
-                      .localizedStrings['Add a Screenshot (optional)'] ??
-                  'Add a Screenshot (optional)'),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: colorWhite,
-                backgroundColor: firstMainColor,
-                side: BorderSide(color: firstMainColor),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                minimumSize: Size(double.infinity,
-                    size.height * 0.06), // Responsive button size
-              ),
-            ),
+            image != null
+                ? Center(
+                    child: GestureDetector(
+                        onTap: () {
+                          selectImage();
+                        },
+                        child: CircleAvatar(
+                            radius: 59, backgroundImage: MemoryImage(image!))))
+                : ElevatedButton.icon(
+                    onPressed: () {
+                      selectImage();
+                    },
+                    icon: Icon(Icons.add_photo_alternate_outlined),
+                    label: Text(languageProvider
+                            .localizedStrings['Add a Screenshot (optional)'] ??
+                        'Add a Screenshot (optional)'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: colorWhite,
+                      backgroundColor: firstMainColor,
+                      side: BorderSide(color: firstMainColor),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      minimumSize: Size(double.infinity,
+                          size.height * 0.06), // Responsive button size
+                    ),
+                  ),
             Spacer(), // Push the send button to the bottom
             // Send button
             SizedBox(
               width: double.infinity,
               height: size.height * 0.08, // Responsive button height
               child: SaveButton(
-                onTap: () {
+                onTap: () async {
                   // Handle sending message
+                  if (_messageController.text.isEmpty) {
+                    showMessageBar("Text is Required", context);
+                  } else {
+                    setState(() {
+                      isloading = true;
+                    });
+                    String photo = await StorageMethods()
+                        .uploadImageToStorage("childName", image!);
+                    FirebaseFirestore.instance
+                        .collection("help")
+                        .doc(uuid)
+                        .set({
+                      "uuid": uuid,
+                      "message": _messageController.text,
+                      "image": photo ?? "",
+                    });
+                    setState(() {
+                      isloading = false;
+                    });
+                    showMessageBar("Report is Send", context);
+                  }
                 },
                 title: languageProvider.localizedStrings['Send'] ?? "Send",
               ),
@@ -92,5 +134,12 @@ class _HelpSupportState extends State<HelpSupport> {
         ),
       ),
     );
+  }
+
+  selectImage() async {
+    Uint8List ui = await pickImage(ImageSource.gallery);
+    setState(() {
+      image = ui;
+    });
   }
 }
--- lib/screen/profile/report_account.dart
+++ lib/screen/profile/report_account.dart
@@ -1,7 +1,14 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connectingfamilies/service/storage.dart';
 import 'package:connectingfamilies/uitls/colors.dart';
+import 'package:connectingfamilies/uitls/image_picker.dart';
 import 'package:connectingfamilies/widget/save_button.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 import '../../provider/language_provider.dart';
 
@@ -13,6 +20,11 @@ class ReportAccount extends StatefulWidget {
 }
 
 class _ReportAccountState extends State<ReportAccount> {
+  var uuid = Uuid().v4();
+
+  TextEditingController _messageController = TextEditingController();
+  Uint8List? image;
+  bool isloading = false;
   @override
   Widget build(BuildContext context) {
     final languageProvider = Provider.of<LanguageProvider>(context); // Access
@@ -51,6 +63,7 @@ class _ReportAccountState extends State<ReportAccount> {
             SizedBox(height: 16),
             // TextField for message input
             TextField(
+              controller: _messageController,
               maxLines: 6,
               decoration: InputDecoration(
                 hintText:
@@ -63,31 +76,61 @@ class _ReportAccountState extends State<ReportAccount> {
             ),
             SizedBox(height: 16),
             // Button for adding a screenshot
-            ElevatedButton.icon(
-              onPressed: () {
-                // Handle adding screenshot
-              },
-              icon: Icon(Icons.add_photo_alternate_outlined),
-              label: Text('Add a Screenshot (optional)'),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: colorWhite,
-                backgroundColor: firstMainColor,
-                side: BorderSide(color: firstMainColor),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                minimumSize: Size(double.infinity,
-                    size.height * 0.06), // Responsive button size
-              ),
-            ),
+            image != null
+                ? Center(
+                    child: GestureDetector(
+                        onTap: () {
+                          selectImage();
+                        },
+                        child: CircleAvatar(
+                            radius: 59, backgroundImage: MemoryImage(image!))))
+                : ElevatedButton.icon(
+                    onPressed: () {
+                      selectImage();
+                    },
+                    icon: Icon(Icons.add_photo_alternate_outlined),
+                    label: Text(languageProvider
+                            .localizedStrings['Add a Screenshot (optional)'] ??
+                        'Add a Screenshot (optional)'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: colorWhite,
+                      backgroundColor: firstMainColor,
+                      side: BorderSide(color: firstMainColor),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      minimumSize: Size(double.infinity,
+                          size.height * 0.06), // Responsive button size
+                    ),
+                  ),
             Spacer(), // Push the send button to the bottom
             // Send button
             SizedBox(
               width: double.infinity,
               height: size.height * 0.08, // Responsive button height
               child: SaveButton(
-                onTap: () {
-                  // Handle sending message
+                onTap: () async {
+                  if (_messageController.text.isEmpty) {
+                    showMessageBar("Text is Required", context);
+                  } else {
+                    setState(() {
+                      isloading = true;
+                    });
+                    String photo = await StorageMethods()
+                        .uploadImageToStorage("childName", image!);
+                    FirebaseFirestore.instance
+                        .collection("report")
+                        .doc(uuid)
+                        .set({
+                      "uuid": uuid,
+                      "message": _messageController.text,
+                      "image": photo ?? "",
+                    });
+                    setState(() {
+                      isloading = false;
+                    });
+                    showMessageBar("Report is Send", context);
+                  }
                 },
                 title: languageProvider.localizedStrings['Send Report'] ??
                     "Send Report",
@@ -97,5 +140,12 @@ class _ReportAccountState extends State<ReportAccount> {
         ),
       ),
     );
+  }
+
+  selectImage() async {
+    Uint8List ui = await pickImage(ImageSource.gallery);
+    setState(() {
+      image = ui;
+    });
   }
 }
